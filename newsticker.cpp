fe66dd60a45c65cb03883ecc038ba64a7378cddf
Wilson Tang <Wil.7ang@gmail.com>
2014-02-26 11:52:43 -0800

    Fix

--- newsticker.cpp
+++ newsticker.cpp
@@ -184,13 +184,15 @@ unsigned char* encodeLetters(const char* str, /*int* colors*/ int color, int len
             {
                 if(tinyColor == 1 || tinyColor == 3)
                 {
-                    if(currentRow >= 16 && tinystr[currentStringPositionTiny] != '4')
+                    if(currentRow == 16 && tinystr[currentStringPositionTiny] == '4'){}
+                    else
                         valR |= targafont[currentRow][tinyCharacterDictionary[tinystr[currentStringPositionTiny]].first+currentLetterPositionTiny];
                 }
 
                 if(tinyColor == 2 || tinyColor == 3)
                 {
-                    if(currentRow >= 16 && tinystr[currentStringPositionTiny] != '4')
+                    if(currentRow == 16 && tinystr[currentStringPositionTiny] == '4'){}
+                    else
                         valG |= targafont[currentRow][tinyCharacterDictionary[tinystr[currentStringPositionTiny]].first+currentLetterPositionTiny];
                 }
 
