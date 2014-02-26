c8b303fcce019f51f6691b3871bdaf2f78ffd639
Wilson Tang <Wil.7ang@gmail.com>
2014-02-26 08:21:39 -0800

    Fix std typo

--- newsticker.cpp
+++ newsticker.cpp
@@ -116,7 +116,7 @@ char reverseBits(char x)
     return x;
 }
 
-unsigned char* encodeLetters(const char* str, /*int* colors*/ int color, int length, int offset, int currentRow, int &lastFirstLetter, int &curWidthSum, td::map<char, std::pair<int, int> > &characterDictionary)
+unsigned char* encodeLetters(const char* str, /*int* colors*/ int color, int length, int offset, int currentRow, int &lastFirstLetter, int &curWidthSum, std::map<char, std::pair<int, int> > &characterDictionary)
 {
     unsigned char *buffer = new unsigned char[COLUMN_DRIVERS * 2 + 3];
     if(currentRow > 12)
